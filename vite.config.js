02f54807631c1763151671949870cfc0c3f0afcc
Tomas Gorrini Rech <tgorrinirech@gmail.com>
2025-05-16 18:22:21 -0300

    pruebas 1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    watch: {
+      // activa polling en lugar de inotify
+      usePolling: true,
+      // cada cuánto (en ms) mira cambios
+      interval: 100,
+    }
+  }
 })
