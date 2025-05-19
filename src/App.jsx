160527391e491798f8089de03ba957a5fb59eb5f
Agustin2911 <agustinezequielromer2004@gmail.com>
2025-05-19 17:35:20 -0300

    pagina de showproducts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage.jsx";
 import Signup from "./Signup";
 import Register from "./Register";
+import ShowProductsPage from "./ShowProductsPage";
+
 function App() {
   return (
     <>
@@ -11,6 +13,7 @@ function App() {
           <Route path="/" element={<LandingPage />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
           <Route path="/register" element={<Register />}></Route>
+          <Route path="/products" element={<ShowProductsPage />}></Route>
         </Routes>
       </Router>
     </>
---
+++ src/ShowProductsPage.jsx
@@ -0,0 +1,62 @@
+import "./App.css";
+import MainNavbar from "src/components/MainNavbar";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { Box, Flex } from "@chakra-ui/react";
+import ShowProducts from "./components/ShowProducts";
+import { Row } from "reactstrap";
+import Filters from "./components/Filters";
+import { Breadcrumb, For, Stack } from "@chakra-ui/react";
+
+function ShowProductsPage() {
+  const handleApplyFilters = (filters) => {
+    // Acá hacés el filtrado con los valores de filters (ej: sort, minPrice, maxPrice)
+    console.log("Aplicando filtros:", filters);
+  };
+  return (
+    <Box bg={"#170d20"}>
+      <MainNavbar></MainNavbar>
+      <Stack mt={"30px"} ml={"50px"}>
+        <Breadcrumb.Root>
+          <Breadcrumb.List>
+            <Breadcrumb.Item>
+              <Breadcrumb.Link
+                href="#"
+                fontSize="xl"
+                color={"white"}
+                textDecoration={"none"}
+              >
+                Docs
+              </Breadcrumb.Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Separator />
+            <Breadcrumb.Item>
+              <Breadcrumb.Link
+                href="#"
+                fontSize="xl"
+                color={"white"}
+                textDecoration={"none"}
+              >
+                Components
+              </Breadcrumb.Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Separator />
+            <Breadcrumb.Item>
+              <Breadcrumb.CurrentLink fontSize="xl">
+                Props
+              </Breadcrumb.CurrentLink>
+            </Breadcrumb.Item>
+          </Breadcrumb.List>
+        </Breadcrumb.Root>
+      </Stack>
+      <Box display={{ base: "block ", md: "flex" }}>
+        <Filters onApplyFilters={handleApplyFilters}></Filters>
+        <ShowProducts></ShowProducts>
+      </Box>
+      <Footer></Footer>
+    </Box>
+  );
+}
+
+export default ShowProductsPage;
