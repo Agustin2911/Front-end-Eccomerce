c096704d795872a8795785ded8d18c4806ef7f99
PatricioFunes <pfunes@uade.edu.ar>
2025-06-02 18:16:36 -0300

    modificacion de app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ function App() {
           ></Route>
           <Route path="/us" element={<AboutUsPage cart={Cart} />}></Route>
           <Route 
-            path="/mispedidos" 
+            path="/" 
             element={<MisPedidos token={Token_usuario} />}
           ></Route>
         </Routes>
